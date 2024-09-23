dd4288c361a2e43f173917e805c957ded9c5ff5c
RithwikChittineni <rithwikchittineni@gmail.com>
2024-09-22 19:28:25 -0500

    bucket feature

--- awsfrontend/src/ResultsPage.js
+++ awsfrontend/src/ResultsPage.js
@@ -19,9 +19,12 @@ const ResultsPage = () => {
   const [photoURL, setPhotoURL] = useState(null);
   const webcamRef = useRef(null);  // React Webcam ref
 
-  //key goes here
- 
-
+  AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+  
   const s3 = new AWS.S3();
   const bucketName = 'polly-wav';
   
